0005ddeb4c10d350c75542a3c8d853a7d9202447
bilalsaeed <bilalsaeed781833@gmail.com>
2021-04-28 17:59:26 +0500

    Removed Error from main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(d
+      theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: Login(),
