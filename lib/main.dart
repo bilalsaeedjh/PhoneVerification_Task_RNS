7436feb68fc7b2e80a0dd4d93509bf9a3cfc60da
bilalsaeed <bilalsaeed781833@gmail.com>
2021-04-28 18:11:52 +0500

    Changed title of Material App

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Phone Authentication',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
